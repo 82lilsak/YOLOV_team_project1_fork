d4bbd599fa9c16647368db18fcd73c328cbb8f12
wjgk0822 <35884296+wjgk0822@users.noreply.github.com>
2023-11-13 19:49:05 +0900

    Update handtracking module+include YOLOV8
    Update handtracking module+include YOLOV8 pose module

--- music_module.py
+++ music_module.py
@@ -2,7 +2,9 @@ from pydub import AudioSegment
 from pydub.playback import play
 from queue import Queue
 
-music_path = "C:/Users/wjgk0/Downloads/Project_Gesture_Volume_Control_Recording/YOLOV_team_project1/downloaded_audio/萩原 雪歩 - Agape (cover).mp3"
+music_path = "D:/AI_exam/YOLOV_team_project/downloaded_audio/萩原 雪歩 - Agape (cover).mp3"
+
+music_path2="D:/AI_exam/YOLOV_team_project/downloaded_audio/『Lyrics AMV』Boku no Kokoro no Yabai Yatsu【Shayou - Yorushika】Opening Full.mp3"
 
 play_music=False
 def play_music_thread(queue):
@@ -15,5 +17,21 @@ def play_music_thread(queue):
 
         if play_music:
             sound = AudioSegment.from_mp3(music_path)
+            play(sound)
+            play_music = False
+
+
+def play_music_thread2(queue):
+
+    global play_music
+
+    play_music = False
+
+    while True:
+        if not queue.empty():
+            play_music = True
+
+        if play_music:
+            sound = AudioSegment.from_mp3(music_path2)
             play(sound)
             play_music = False
---
+++ new_gesture_main.py
@@ -0,0 +1,256 @@
+import cv2
+import time,  math, numpy as np
+import new_gesture_test as htm
+import pyautogui,autopy
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from music_module import play_music
+from speech_recog_module import speech_recognition_thread
+from music_module import play_music_thread
+#from mouse_module import move_mouse_thread
+from spotify_module import initialize_spotify
+#import pyautogui
+import math
+from yolov8_module import main
+from queue import Queue
+import threading
+import webbrowser
+
+cv2.ocl.setUseOpenCL(True)
+cv2.setUseOptimized(True)
+
+wCam, hCam = 640, 480
+cap = cv2.VideoCapture(0)
+cap.set(3,wCam)
+cap.set(4,hCam)
+pTime = 0
+#cTime = 0
+
+# Queue를 생성하여 스레드 간 통신
+speech_queue = Queue()
+music_queue = Queue()
+
+speech_thread = threading.Thread(target=speech_recognition_thread, args=(speech_queue,))
+music_play_thread = threading.Thread(target=play_music_thread, args=(music_queue,))
+
+speech_thread.daemon = True
+music_play_thread.daemon=True
+
+speech_thread.start()
+music_play_thread.start()
+
+sp=None
+
+detector = htm.handDetector(maxHands=1, detectionCon=0.85, trackCon=0.8)
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+volRange = volume.GetVolumeRange()   #(-63.5, 0.0, 0.5) min max
+
+minVol = -63
+maxVol = volRange[1]
+print(volRange)
+hmin = 50
+hmax = 200
+volBar = 400
+volPer = 0
+vol = 0
+color = (0,215,255)
+
+tipIds = [4, 8, 12, 16, 20]
+mode = ''
+active = 0
+
+pyautogui.FAILSAFE = False
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+   # print(lmList)
+    fingers = []
+
+    music_queue.put(True)
+
+    # 음성 명령을 확인
+    while not speech_queue.empty():
+        command = speech_queue.get()
+        # if "play" in command:
+        #     music_queue.put(True)
+
+        if "search" in command:
+            # 검색할 때 사용할 검색 엔진을 설정 (여기서는 구글)
+            search_engine = "https://www.google.com/search?q="
+
+            # 검색어 추출 (예: "search Python programming")
+            search_query = " ".join(command.split()[1:])
+
+            # 웹 브라우저로 검색 열기
+            webbrowser.open(search_engine + search_query)
+
+        elif "spotify" in command:
+            sp = initialize_spotify()
+
+        elif "yolo" in command:
+
+            cap.release()
+            cv2.destroyAllWindows()
+
+            main()
+            cap = cv2.VideoCapture(0)
+            cap.set(3, wCam)
+            cap.set(4, hCam)
+
+
+    if len(lmList) != 0:
+
+        #Thumb
+        if lmList[tipIds[0]][1] > lmList[tipIds[0 -1]][1]:
+            if lmList[tipIds[0]][1] >= lmList[tipIds[0] - 1][1]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        elif lmList[tipIds[0]][1] < lmList[tipIds[0 -1]][1]:
+            if lmList[tipIds[0]][1] <= lmList[tipIds[0] - 1][1]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        for id in range(1,5):
+            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+
+      #  print(fingers)
+        if (fingers == [0,0,0,0,0]) & (active == 0 ):
+            mode='N'
+        elif (fingers == [0, 1, 0, 0, 0] or fingers == [0, 1, 1, 0, 0]) & (active == 0 ):
+            mode = 'Scroll'
+            active = 1
+        elif (fingers == [1, 1, 0, 0, 0] ) & (active == 0 ):
+             mode = 'Volume'
+             active = 1
+        elif (fingers == [1 ,1 , 1, 1, 1] ) & (active == 0 ):
+             mode = 'Cursor'
+             active = 1
+
+############# Scroll 👇👇👇👇##############
+    if mode == 'Scroll':
+        active = 1
+     #   print(mode)
+        putText(mode)
+        cv2.rectangle(img, (200, 410), (245, 460), (255, 255, 255), cv2.FILLED)
+        if len(lmList) != 0:
+            if fingers == [0,1,0,0,0]:
+              #print('up')
+              #time.sleep(0.1)
+                putText(mode = 'U', loc=(200, 455), color = (0, 255, 0))
+                pyautogui.scroll(300)
+
+            if fingers == [0,1,1,0,0]:
+                #print('down')
+              #  time.sleep(0.1)
+                putText(mode = 'D', loc =  (200, 455), color = (0, 0, 255))
+                pyautogui.scroll(-300)
+            elif fingers == [0, 0, 0, 0, 0]:
+                active = 0
+                mode = 'N'
+################# Volume 👇👇👇####################
+    if mode == 'Volume':
+        active = 1
+       #print(mode)
+        putText(mode)
+        if len(lmList) != 0:
+            if fingers[-1] == 1:
+                active = 0
+                mode = 'N'
+                print(mode)
+
+            else:
+
+                 #   print(lmList[4], lmList[8])
+                    x1, y1 = lmList[4][1], lmList[4][2]
+                    x2, y2 = lmList[8][1], lmList[8][2]
+                    cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+                    cv2.circle(img, (x1, y1), 10, color, cv2.FILLED)
+                    cv2.circle(img, (x2, y2), 10, color, cv2.FILLED)
+                    cv2.line(img, (x1, y1), (x2, y2), color, 3)
+                    cv2.circle(img, (cx, cy), 8, color, cv2.FILLED)
+
+                    length = math.hypot(x2 - x1, y2 - y1)
+                    # print(length)
+
+                    # hand Range 50-300
+                    # Volume Range -65 - 0
+                    vol = np.interp(length, [hmin, hmax], [minVol, maxVol])
+                    volBar = np.interp(vol, [minVol, maxVol], [400, 150])
+                    volPer = np.interp(vol, [minVol, maxVol], [0, 100])
+                    print(vol)
+                    volN = int(vol)
+                    if volN % 4 != 0:
+                        volN = volN - volN % 4
+                        if volN >= 0:
+                            volN = 0
+                        elif volN <= -64:
+                            volN = -64
+                        elif vol >= -11:
+                            volN = vol
+
+                #    print(int(length), volN)
+                    volume.SetMasterVolumeLevel(vol, None)
+                    if length < 50:
+                        cv2.circle(img, (cx, cy), 11, (0, 0, 255), cv2.FILLED)
+
+                    cv2.rectangle(img, (30, 150), (55, 400), (209, 206, 0), 3)
+                    cv2.rectangle(img, (30, int(volBar)), (55, 400), (215, 255, 127), cv2.FILLED)
+                    cv2.putText(img, f'{int(volPer)}%', (25, 430), cv2.FONT_HERSHEY_COMPLEX, 0.9, (209, 206, 0), 3)
+
+
+#######################################################################
+    if mode == 'Cursor':
+        active = 1
+        #print(mode)
+        putText(mode)
+        cv2.rectangle(img, (110, 20), (620, 350), (255, 255, 255), 3)
+
+        if fingers[1:] == [0,0,0,0]: #thumb excluded
+            active = 0
+            mode = 'N'
+            print(mode)
+        else:
+            if len(lmList) != 0:
+                x1, y1 = lmList[8][1], lmList[8][2]
+                w, h = autopy.screen.size()
+                X = int(np.interp(x1, [110, 620], [0, w - 1]))
+                Y = int(np.interp(y1, [20, 350], [0, h - 1]))
+                cv2.circle(img, (lmList[8][1], lmList[8][2]), 7, (255, 255, 255), cv2.FILLED)
+                cv2.circle(img, (lmList[4][1], lmList[4][2]), 10, (0, 255, 0), cv2.FILLED)  #thumb
+
+                if X%2 !=0:
+                    X = X - X%2
+                if Y%2 !=0:
+                    Y = Y - Y%2
+                print(X,Y)
+                autopy.mouse.move(X,Y)
+              #  pyautogui.moveTo(X,Y)
+                if fingers[0] == 0:
+                    cv2.circle(img, (lmList[4][1], lmList[4][2]), 10, (0, 0, 255), cv2.FILLED)  # thumb
+                    pyautogui.click()
+
+    cTime = time.time()
+    fps = 1/((cTime + 0.01)-pTime)
+    pTime = cTime
+
+    cv2.putText(img,f'FPS:{int(fps)}',(480,50), cv2.FONT_ITALIC,1,(255,0,0),2)
+    cv2.imshow('Hand LiveFeed',img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    def putText(mode,loc = (250, 450), color = (0, 255, 255)):
+        cv2.putText(img, str(mode), loc, cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                    3, color, 3)
